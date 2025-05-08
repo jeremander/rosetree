d6e032a1b939bdf9b985cb4e3f460da5de09a96b
Jeremy Silver <jeremys@nessiness.com>
2025-05-08 18:24:03 -0400

    feat: add trie submodule with Trie class

--- rosetree/__init__.py
+++ rosetree/__init__.py
@@ -1,3 +1,14 @@
 """Library implementing the "rose tree" data structure."""
 
+from .tree import FrozenTree, MemoTree, Tree
+from .trie import Trie
+
+
 __version__ = '0.1.0'
+
+__all__ = [
+    'FrozenTree',
+    'MemoTree',
+    'Tree',
+    'Trie',
+]
