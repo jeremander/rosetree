bd936ac98e26a7a4efa0dd88ba5f254e7d915d3c
Jeremy Silver <jeremys@nessiness.com>
2025-05-12 21:29:22 -0400

    refactor: revise pretty tree-drawing algorithm for better alignment

--- rosetree/draw.py
+++ rosetree/draw.py
@@ -1,6 +1,7 @@
 """This module contains algorithms for drawing trees prettily."""
 
 from collections.abc import Sequence
+from math import ceil
 import re
 from typing import NamedTuple, Optional, TypeVar
 
@@ -10,6 +11,8 @@ from .tree import BaseTree
 
 
 T = TypeVar('T')
+
+_PARTITION_REGEX = re.compile(r'^(\s*)([^\s](.*[^\s])?)(\s*)$')
 
 
 # LONG FORMAT
@@ -41,8 +44,6 @@ def _center_index(n: int) -> int:
     """Gets the (integer) midpoint index for the given distance n."""
     return max(0, n - 1) // 2
 
-_PARTITION_REGEX = re.compile(r'^(\s*)([^\s](.*[^\s])?)(\s*)$')
-
 def _partition_line(line: str) -> tuple[str, str, str]:
     """Giving a line with leading and/or trailing whitespace, splits it into these three parts, returning a tuple (leading whitespace, text, trailing whitespace)."""
     (leading, text, _, trailing) = _PARTITION_REGEX.match(line).groups()  # type: ignore[union-attr]
@@ -57,6 +58,14 @@ def _place_line(line: str, width: int, center: int) -> str:
     if (lpad < 0) or (rpad < 0):
         raise ValueError(f'cannot center line of length {line_length} at index {center} in a width of {width}')
     return (' ' * lpad) + line + (' ' * rpad)
+
+def _pad_lines(lines: list[str]) -> list[str]:
+    # ensure the tree is padded on the left & right
+    if any(not line.startswith(' ') for line in lines):
+        lines = [' ' + line for line in lines]
+    if any(not line.endswith(' ') for line in lines):
+        lines = [line + ' ' for line in lines]
+    return lines
 
 def _get_box_char(j: int, lcenter: int, midpoint: int, rcenter: int) -> str:
     if j == midpoint:
@@ -88,6 +97,7 @@ class Column(NamedTuple):
         """Conjoint multiple columns horizontally into one.
         An integer, spacing, specifies how many spaces to insert between each column.
         If top_down = True, aligns conjoined columns from the top, otherwise from the bottom."""
+        assert len(columns) > 1
         (widths, _, cols) = zip(*columns)
         heights = [len(col) for col in cols]
         max_height = max(heights)
@@ -96,89 +106,84 @@ class Column(NamedTuple):
             cols = tuple(col + empty for (empty, col) in zip(empty_rows, cols))
         else:
             cols = tuple(empty + col for (empty, col) in zip(empty_rows, cols))
-        if len(columns) == 1:  # pad the outside
-            width = widths[0] + 2 * (spacing - 1)
-            delim = ' ' * (spacing - 1)
-            col = [delim + row + delim for row in cols[0]]
-        else:
-            width = sum(widths) + (spacing * (len(cols) - 1))
-            delim = ' ' * spacing
-            col = [delim.join(row) for row in zip(*cols)]
+        width = sum(widths) + (spacing * (len(cols) - 1))
+        delim = ' ' * spacing
+        col = [delim.join(row) for row in zip(*cols)]
         return cls(width, _center_index(width), col)
 
+    def pad_to(self, width: int) -> Self:
+        """Pads the column to the given width."""
+        n = width - self.width
+        if n <= 0:
+            return self
+        lpad = ' ' * (n // 2)
+        rpad = ' ' * (n - n // 2)
+        rows = [lpad + row + rpad for row in self.rows]
+        return type(self)(width, _center_index(width), rows)
 
-def pretty_tree_wide(tree: BaseTree[T], *, top_down: bool = False) -> str:
+
+def pretty_tree_wide(tree: BaseTree[T], *, top_down: bool = False, spacing: int = 2) -> str:
     """Given a tree whose nows can be converted to strings via `str`, produces a pretty rendering of that tree in "wide format."
     This presents the tree's root node at the top, with branches cascading down.
     If top_down = True, positions nodes of the same depth on the same vertical level.
-    Otherwise, positions leaf nodes on the same vertical level."""
+    Otherwise, positions leaf nodes on the same vertical level.
+    spacing is an integer indicating the minimum number of spaces between each column."""
     def conjoin_subtrees(node: T, children: Sequence[Column]) -> Column:
         node_str = str(node)
         node_lines = node_str.splitlines() if node_str else ['']
         node_width = max(map(len, node_lines))
         node_lines = [line.ljust(node_width) for line in node_lines]
-        if (num_children := len(children)) == 0:
+        if (num_children := len(children)) == 0:  # leaf
             return Column(node_width, _center_index(node_width), node_lines)
         (child_widths, child_centers, _) = zip(*children)
-        spacing = 2
-        while True:
-            (width, center, text) = Column.conjoin(children, spacing, top_down)
-            # place parent at the midpoint of the left and right childrens' centers
-            if num_children == 1:
-                spans = [(0, width)]
-                centers = [child_centers[0] + (spacing - 1)]
-            else:
-                spans = [(0, child_widths[0])]
-                for child_width in child_widths[1:]:
-                    start = spans[-1][1]
-                    spans.append((start + spacing, start + spacing + child_width))
-                centers = [span[0] + child_center for (span, child_center) in zip(spans, child_centers)]
+        if num_children == 1:
+            (width, center, rows) = children[0]
+            spans = [(0, width)]
+            centers = [center]
+            midpoint = center
+            edges = ['│' if (j == center) else ' ' for j in range(width)]
+        else:
+            # calculate the smallest spacing required for the child width to exceed the parent width
+            num_spaces = max(spacing, ceil((node_width - sum(child_widths)) / (num_children - 1)))
+            (width, center, rows) = Column.conjoin(children, num_spaces, top_down)
+            # place parent at the midpoint of the leftmotst and rightmost child's centers
+            spans = [(0, child_widths[0])]
+            for child_width in child_widths[1:]:
+                start = spans[-1][1]
+                spans.append((start + num_spaces, start + num_spaces + child_width))
+            centers = [start + child_center for ((start, _), child_center) in zip(spans, child_centers)]
             (lcenter, rcenter) = (centers[0], centers[-1])
             midpoint = lcenter + _center_index(rcenter - lcenter + 1)
-            try:
-                node_lines = [_place_line(line, width, midpoint) for line in node_lines]
-                break
-            except ValueError:
-                # parent is too wide for the children: add more space to children
-                spacing += 1  # TODO: can we calculate the right value up-front?
-        if num_children == 1:
-            edges = ['│' if (j == midpoint) else ' ' for j in range(width)]
-        else:
             edges = [_get_box_char(j, lcenter, midpoint, rcenter) for j in range(width)]
-            text = [' ' * width] + text
+            for j in centers:
+                edges[j] = _extend_box_char_down(edges[j])
+        node_lines = [_place_line(line, width, midpoint) for line in node_lines]
         node_lines.append(''.join(edges))
         # get mapping from center indices to column spans
+        # TODO: seems like this could be done more efficiently
         def find_span(center: int) -> Optional[tuple[int, int]]:
             return next(((start, stop) for (start, stop) in spans if start <= center < stop), None)
         column_spans = {c: find_span(c) for c in centers}
         # insert '|' downward to each child
-        text_cols = [list(col) for col in zip(*text)]
-        extension_indices = set()  # indices at which to branch downward
+        text_cols = [list(col) for col in zip(*rows)]
+        # extension_indices = set()  # indices at which to branch downward
         for (j, col) in enumerate(text_cols):
             if (span := column_spans.get(j, None)) is None:
                 continue
-            for (i, row) in enumerate(text):
+            for (i, row) in enumerate(rows):
                 if row[span[0]:span[1]].isspace():
                     col[i] = '│'
-                    if i == 0:
-                        extension_indices.add(j)
                 else:
                     break
-        if extension_indices:
-            # branch downward from horizontal lines
-            node_lines[1] = ''.join([_extend_box_char_down(c) if (j in extension_indices) else c for (j, c) in enumerate(node_lines[1])])
-        text = node_lines + [''.join(row) for row in zip(*text_cols)]
+        rows = node_lines + [''.join(row) for row in zip(*text_cols)]
         top_line = node_lines[0]
         if top_line.isspace():
             new_center = center
         else:
             (leading, top_text, _) = _partition_line(node_lines[0])
             new_center = len(leading) + _center_index(len(top_text))
-        return Column(width, new_center, text)
+        return Column(width, new_center, rows)
     lines = tree.fold(conjoin_subtrees).rows
     # ensure the tree is padded on the left & right
-    if any(not line.startswith(' ') for line in lines):
-        lines = [' ' + line for line in lines]
-    if any(not line.endswith(' ') for line in lines):
-        lines = [line + ' ' for line in lines]
-    return '\n'.join(lines) + '\n'
+    lines = _pad_lines(lines)
+    return '\n'.join(lines)
--- tests/test_draw.py
+++ tests/test_draw.py
@@ -5,36 +5,226 @@ from rosetree import Tree
 
 Tr = Tree
 
-TREE1 = Tr(0, [Tr(1), Tr(2, [Tr(3, [Tr(4, [Tr(5)])]), Tr(6, [Tr(7), Tr(8)]), Tr(9)]), Tr(10)])
-
-TREE1_PRETTY = {
-    'bottom-up': """
-          0
- ┌────────┴┬────────┐
- │         │        │
- │         2        │
- │    ┌────┴┬────┐  │
- │    │     │    │  │
- │    3     │    │  │
- │    │     6    │  │
- │    4    ┌┴─┐  │  │
- │    │    │  │  │  │
- 1    5    7  8  9  10
+PRETTY_TREES = [
+    {
+        'tree': Tr(0),
+        'bottom-up': ' 0 ',
+        'top-down': ' 0 ',
+        'long': '0',  # TODO: should this be distinguished somehow?
+    },
+    {
+        'tree': Tr(0, [Tr(1)]),
+        'bottom-up': """
+ 0
+ │
+ 1
+ """,
+        'top-down': """
+ 0
+ │
+ 1
+ """ ,
+        'long': """
+0
+└── 1
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1, [Tr(2)])]),
+        'bottom-up': """
+ 0
+ │
+ 1
+ │
+ 2
+ """,
+        'top-down': """
+ 0
+ │
+ 1
+ │
+ 2
+ """ ,
+        'long': """
+0
+└── 1
+    └── 2
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1), Tr(2)]),
+        'bottom-up': """
+  0
+ ┌┴─┐
+ 1  2
 """,
-    'top-down': """
-          0
- ┌────────┴┬────────┐
- │         │        │
- 1         2        10
-      ┌────┴┬────┐
-      │     │    │
-      3     6    9
-      │    ┌┴─┐
-      4    │  │
-      │    7  8
-      5
+        'top-down': """
+  0
+ ┌┴─┐
+ 1  2
 """,
-    'long': """
+        'long': """
+0
+├── 1
+└── 2
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1), Tr(2222222)]),
+        'bottom-up': """
+    0
+ ┌──┴──┐
+ 1  2222222
+""",
+        'top-down': """
+    0
+ ┌──┴──┐
+ 1  2222222
+""",
+        'long': """
+0
+├── 1
+└── 2222222
+"""
+    },
+    {
+        'tree': Tr(123456789, [Tr(1), Tr(2)]),
+        'bottom-up': """
+ 123456789
+ ┌───┴───┐
+ 1       2
+""",
+        'top-down': """
+ 123456789
+ ┌───┴───┐
+ 1       2
+""",
+        'long': """
+123456789
+├── 1
+└── 2
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1), Tr(2, [Tr(3)])]),
+        'bottom-up': """
+  0
+ ┌┴─┐
+ │  2
+ │  │
+ 1  3
+""",
+        'top-down': """
+  0
+ ┌┴─┐
+ 1  2
+    │
+    3
+""",
+        'long': """
+0
+├── 1
+└── 2
+    └── 3
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1, [Tr(2)]), Tr(3)]),
+        'bottom-up': """
+  0
+ ┌┴─┐
+ 1  │
+ │  │
+ 2  3
+""",
+        'top-down': """
+  0
+ ┌┴─┐
+ 1  3
+ │
+ 2
+""",
+        'long': """
+0
+├── 1
+│   └── 2
+└── 3
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1), Tr(2), Tr(3)]),
+        'bottom-up': """
+    0
+ ┌──┼──┐
+ 1  2  3
+""",
+        'top-down': """
+    0
+ ┌──┼──┐
+ 1  2  3
+""",
+        'long': """
+0
+├── 1
+├── 2
+└── 3
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1, [Tr(2)]), Tr(3), Tr(4, [Tr(5, [Tr(6)])])]),
+        'bottom-up': """
+    0
+ ┌──┼──┐
+ │  │  4
+ │  │  │
+ 1  │  5
+ │  │  │
+ 2  3  6
+""",
+        'top-down': """
+    0
+ ┌──┼──┐
+ 1  3  4
+ │     │
+ 2     5
+       │
+       6
+""",
+        'long': """
+0
+├── 1
+│   └── 2
+├── 3
+└── 4
+    └── 5
+        └── 6
+"""
+    },
+    {
+        'tree': Tr(0, [Tr(1), Tr(2, [Tr(3, [Tr(4, [Tr(5)])]), Tr(6, [Tr(7), Tr(8)]), Tr(9)]), Tr(10)]),
+        'bottom-up': """
+        0
+ ┌──────┼───────┐
+ │      2       │
+ │  ┌───┼────┐  │
+ │  3   │    │  │
+ │  │   │    │  │
+ │  4   6    │  │
+ │  │  ┌┴─┐  │  │
+ 1  5  7  8  9  10
+""",
+        'top-down': """
+        0
+ ┌──────┼───────┐
+ 1      2       10
+    ┌───┼────┐
+    3   6    9
+    │  ┌┴─┐
+    4  7  8
+    │
+    5
+""",
+        'long': """
 0
 ├── 1
 ├── 2
@@ -47,21 +237,48 @@ TREE1_PRETTY = {
 │   └── 9
 └── 10
 """,
-}
+    },
+    {
+        'tree': Tr(''),
+        'bottom-up': '',
+        'top-down': '',
+        'long': '',
+    },
+    {
+        'tree': Tr('', [Tree(''), Tree(2)]),
+        'bottom-up': """
+
+ ┌┴┐
+   2
+""",
+        'top-down': """
+
+ ┌┴┐
+   2
+""",
+        'long': """
+
+├──
+└── 2
+"""
+    },
+]
 
 def _normalize_pretty(s):
-    return '\n'.join(line.rstrip() for line in s.splitlines())
+    return '\n'.join(line.rstrip() for line in s.splitlines()).rstrip('\n')
 
-@pytest.mark.parametrize(['tree', 'style', 'output'], [
-    pytest.param(TREE1, 'bottom-up', TREE1_PRETTY['bottom-up'], id='tree1_bottom_up'),
-    pytest.param(TREE1, 'top-down', TREE1_PRETTY['top-down'], id='tree1_top_down'),
-    pytest.param(TREE1, 'long', TREE1_PRETTY['long'], id='tree1_long'),
-])
-def test_pretty(tree, style, output):
+@pytest.mark.parametrize('item', PRETTY_TREES)
+def test_pretty(item):
     """Tests pretty string drawing of trees."""
-    assert _normalize_pretty(tree.pretty(style=style)) == output.strip('\n')
+    tree = item['tree']
+    for style in ['bottom-up', 'top-down', 'long']:
+        if style in item:
+            output = item[style]
+            if output.startswith('\n'):
+                output = output[1:]
+            assert _normalize_pretty(tree.pretty(style=style)) == _normalize_pretty(output)
 
 def test_invalid_pretty_style():
     """Tests that a ValueError is raised if an invalid pretty style is provided to Tree.pretty."""
     with pytest.raises(ValueError, match="invalid pretty tree style 'fake'"):
-        _ = TREE1.pretty(style='fake')
+        _ = PRETTY_TREES[0]['tree'].pretty(style='fake')
