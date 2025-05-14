cce975a8521e1b5e104c9ca6db031f95748688c2
Jeremy Silver <jeremys@nessiness.com>
2025-05-14 18:02:12 -0400

    feat: expose TreeDrawOptions and TreeLayoutOptions to top-level package exports

--- rosetree/__init__.py
+++ rosetree/__init__.py
@@ -1,5 +1,6 @@
 """Library implementing the "rose tree" data structure."""
 
+from .draw import TreeDrawOptions, TreeLayoutOptions
 from .tree import FrozenTree, MemoTree, Tree, zip_trees, zip_trees_with
 from .trie import Trie
 
@@ -10,6 +11,8 @@ __all__ = [
     'FrozenTree',
     'MemoTree',
     'Tree',
+    'TreeDrawOptions',
+    'TreeLayoutOptions',
     'Trie',
     'zip_trees',
     'zip_trees_with',
