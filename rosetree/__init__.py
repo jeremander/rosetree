977cd7ec9ff9df3d0ad97af47bdc0b12e9a5f313
Jeremy Silver <jeremys@nessiness.com>
2025-05-17 13:46:03 -0400

    refactor: pass kwargs to figure drawing in Treemap.draw_treemap

--- rosetree/__init__.py
+++ rosetree/__init__.py
@@ -3,9 +3,10 @@
 from .draw import TreeDrawOptions, TreeLayoutOptions
 from .tree import FrozenTree, MemoTree, Tree, zip_trees, zip_trees_with
 from .trie import Trie
+from .weighted import Treemap
 
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 __all__ = [
     'FrozenTree',
@@ -13,6 +14,7 @@ __all__ = [
     'Tree',
     'TreeDrawOptions',
     'TreeLayoutOptions',
+    'Treemap',
     'Trie',
     'zip_trees',
     'zip_trees_with',
